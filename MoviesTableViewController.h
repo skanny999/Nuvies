7dd01fbeee8d39cb45609de419d19f6252babe9e
skanny999 <skanny999@me.com>
2016-05-04 15:35:29 +0100

    first commit

--- MoviesTableViewController.h
+++ MoviesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MoviesTableViewController.h
 //  Nuvies
 //
 //  Created by Riccardo Scanavacca on 04/05/2016.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MoviesTableViewController : UITableViewController
 
 @end
-
